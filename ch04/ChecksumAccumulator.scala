aa06148a7c9d34e57de2f12a5cf09e9f75c480df
skielosky <ezequiel.aguilarg@gmail.com>
2016-07-16 20:45:06 -0500

    Create Ch04 folder

---
+++ ch04/ChecksumAccumulator.scala
@@ -0,0 +1,22 @@
+//In file ChecksumAccumulator.scala
+
+import scala.collection.mutable
+
+object ChecksumAccumulator {
+	private var sum = 0
+      	def add(b: Byte): Unit = { sum += b }
+      	def checksum(): Int = ~(sum & 0xFF) + 1
+	private val cache = mutable.Map.empty[String, Int]
+
+	def calculate(s: String): Int =
+		if(cache.contains(s))
+			cache(s)
+		else {
+			val acc = new ChecksumAccumulator
+			for(c <- s)
+				acc.add(c.toByte)
+			val cs = acc.checksum()
+			cache += (s -> cs)
+			cs		
+		}
+}
---
+++ ch04/Summer.scala
@@ -0,0 +1,12 @@
+// In file Summer.scala
+
+import ChecksumAccumulator.calculate
+
+object Summer {
+	def main(args: Array[String]) = {
+		for(arg <- args)
+			println(arg + ": " + calculate(arg))	
+	}
+
+}
+
