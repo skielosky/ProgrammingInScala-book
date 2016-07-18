2329abf2a34da4793ffc9e4fc58cbdfed5a41230
skielosky <ezequiel.aguilarg@gmail.com>
2016-07-17 22:09:54 -0500

    Rational class

--- ch05/Rational.scala
+++ ch05/Rational.scala
@@ -1,4 +1,22 @@
 class Rational(n: Int, d: Int) {
-    println("Created " + n + "/" + d)
+	require(d != 0)
+	val numer: Int = n
+      	val denom: Int = d
+
+	def this(n: Int) = this(n, 1) // auxiliary constructor
+
+	override def toString = numer + "/" + denom
+	
+	def add(that: Rational): Rational =
+		new Rational(
+			numer * that.denom + that.numer * denom,
+          		denom * that.denom
+		)
+
+	def lessThan(that: Rational) = 
+    		this.numer * that.denom < that.numer * this.denom
+
+	def max(that: Rational) = 
+    		if (this.lessThan(that)) that else this
   }
 
